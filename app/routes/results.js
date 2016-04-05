92aad4c0851ebc4cea879e7464fbd97a476956c2
matt knutson and nic netzel <netzeln@gmail.com>
2016-04-05 11:41:28 -0700

    encoded API key for security

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,8 +1,10 @@
+import config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=f30272e3e9d74359ab7869a25fb93aa1&zip=' + params.zip;
+    var key = config.myApiKey;
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=' +key+ '&zip=' +params.zip;
       return Ember.$.getJSON(url).then(function(responseJSON){
         return responseJSON.results;
       });
--- app/routes/subcommittee.js
+++ app/routes/subcommittee.js
@@ -1,8 +1,10 @@
+import config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(committee) {
-    var url = 'http://congress.api.sunlightfoundation.com/committees?apikey=f30272e3e9d74359ab7869a25fb93aa1&per_page=50&parent_committee_id=' + committee.committee_id;
+    var key = config.myApiKey;
+    var url = 'http://congress.api.sunlightfoundation.com/committees?apikey=' +key+ '&per_page=50&parent_committee_id=' + committee.committee_id;
     console.log(url);
     return Ember.$.getJSON(url).then(function(responseJSON){
       return responseJSON.results;
